2af34ee98a635d039199da6639fe6e42ff99e6c0
FADEL MUHAMAD RIFAI 411221027 <411221027@mahasiswa.undira.ac.id>
2025-08-21 04:56:27 +0000

    implementation PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,33 @@
-import type { Metadata } from "next";
+"use client";
+
+import { useEffect } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "PDF-adel | MERGE, SPLIT, SELECT PDF by faaadelmr",
-  description: "All Progress on Your Browser, NO NEED UPLOAD so it's Secure. Easily select, split, merge, and manage your PDF files. A fast, secure, and CYBERNETIC PDF toolkit. #CobaAjaDulu",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      });
+    }
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <title>PDF-adel | MERGE, SPLIT, SELECT PDF by faaadelmr</title>
+        <meta name="description" content="All Progress on Your Browser, NO NEED UPLOAD so it's Secure. Easily select, split, merge, and manage your PDF files. A fast, secure, and CYBERNETIC PDF toolkit. #CobaAjaDulu" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Rajdhani:wght@400;500;700&display=swap" rel="stylesheet" />
